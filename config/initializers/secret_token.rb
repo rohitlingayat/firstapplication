b78e4cd3152981472cb0df06d95b5d42c0591820
tudip <tudip@tudip-Vostro-2520.(none)>
2013-02-25 18:46:07 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '6af112ebef366522b6f722032a4a366d015ec0799336247ef1e73ccfd2eee9fe15c394317e6281c108b7f7090ac56d6dd12abc877fb48b78ae31249ad9d1edae'
